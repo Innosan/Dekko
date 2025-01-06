d6934b4c4b3dd70135f0d699b438a0979918f9aa
Mikhail <90400368+Innosan@users.noreply.github.com>
2025-01-06 23:09:15 +0300

    Initialize netlify

---
+++ components/layout/forms/TitledBlock.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps({
+	title: {
+		type: String,
+		required: true,
+	},
+	spacing: {
+		type: String as PropType<'gap-1' | 'gap-2' | 'gap-3' | 'gap-4'>,
+		default: 'gap-1',
+	},
+});
+</script>
+
+<template>
+	<div
+		class="flex flex-col"
+		:class="props.spacing"
+	>
+		<p class="opacity-70 text-sm">{{ title }}</p>
+		<slot />
+	</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { useAnimeStore } from '~/stores/anime';
+
+const animeStore = useAnimeStore();
+animeStore.getArts(false);
+</script>
+
+<template>
+	<PageSection
+		title="Welcome to the homepage"
+		icon="i-heroicons-home-solid"
+		:is-divided="false"
+	>
+		<UButtonGroup>
+			<UButton
+				v-for="tag in animeStore.categories"
+				:key="tag"
+				variant="subtle"
+				:label="tag"
+				@click="animeStore.getArtsByTag(tag)"
+			/>
+		</UButtonGroup>
+
+		<Grid v-auto-animate>
+			<ImageCard
+				v-for="image in animeStore.nekoArts"
+				:key="image.url"
+				:alt="image.artist_name"
+				:url="image.url"
+			>
+				<UButtonGroup>
+					<UButton
+						class="font-bold"
+						:to="image.artist_href || '#'"
+						variant="subtle"
+						:label="image.artist_name || 'Unknown'"
+						target="_blank"
+					/>
+
+					<UButton
+						v-if="image.source_url"
+						:to="image.source_url"
+						:external="true"
+						class="text-xs"
+						label="Source"
+						variant="subtle"
+						target="_blank"
+					/>
+				</UButtonGroup>
+			</ImageCard>
+		</Grid>
+
+		<div class="flex gap-4">
+			<UButton
+				label="Load more"
+				icon="i-heroicons-chevron-down"
+				:loading="animeStore.isRefilling"
+				@click="animeStore.getArts(true)"
+			/>
+
+			<UButton
+				label="Refresh"
+				icon="i-heroicons-arrow-path"
+				:loading="animeStore.isLoading"
+				@click="animeStore.getArts(false)"
+			/>
+		</div>
+	</PageSection>
+</template>
